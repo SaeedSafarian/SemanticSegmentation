135d7a09d17012e3558dc8e44be1615e2825a19e
SaeedSafarian <45786947+SaeedSafarian@users.noreply.github.com>
2020-03-09 18:49:38 +0330

    Update EliminateBackground.py

--- EliminateBackground.py
+++ EliminateBackground.py
@@ -1,17 +1,9 @@
 path = input('Inpur URL')
 
 def Eliminating_Background(path):
-    --trained_model ='./ SemanticSegmentation / weights / yolact_plus_resnet50_54_800000.pth'
-    --config = yolact_plus_resnet50_config
-    --score_threshold = 0.15
-    --top_k = 15
-    --display_masks = true
-    --display_bboxes = false
-    --display_text = false
-    --images = test_images:output_images
     mydir = os.getcwd()
     mydir_new = os.chdir(mydir+"/SemanticSegmentation")
-    exec(open('./eval_v2.py').read())
+    exec(open('./eval_v3.py').read())
     print ("Hello World!")
     output_images = output_images.save("Eliminated_background.jpg")
 if __name__== "__main__":
